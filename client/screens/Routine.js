6dd8cee53a90129a6f192e07f75a84de84ce84d5
AdamMalm <adam.hornemalm@gmail.com>
2022-12-13 10:34:44 +0100

    test

--- client/screens/Routine.js
+++ client/screens/Routine.js
@@ -1,6 +1,6 @@
 // Apollo exempel
-import { gql, useQuery } from '@apollo/client'
-import { Text, View } from 'react-native';
+import { gql, useQuery } from "@apollo/client";
+import { Text, View } from "react-native";
 
 const GET_USERS = gql`
     query Users {
@@ -11,20 +11,20 @@ const GET_USERS = gql`
     }
 `;
 
-
 const Routine = () => {
   const { loading, error, data } = useQuery(GET_USERS);
 
-  if (loading) return <Text>Loading...</Text>
-  if (error) return <Text>{console.log(error)}Error T_T</Text>
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>{console.log(error)}Error T_T</Text>;
 
   return (
     <>
-        <View>
-            <Text>{console.log(data)}hej</Text>
-        </View>
+      <View>
+        <Text>{console.log(data)}hej</Text>
+        <Text>{console.log(data)}hej</Text>
+      </View>
     </>
-  )
-}
+  );
+};
 
-export default Routine
+export default Routine;
