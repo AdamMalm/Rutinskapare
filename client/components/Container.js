125598a2d53bcd22d671ba964da37b466630dd48
Ebba Wåhlin <ebba.wahlin@icloud.com>
2022-12-14 16:00:08 +0100

    create routine screen and routine component

--- client/components/Container.js
+++ client/components/Container.js
@@ -1,10 +1,10 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { StyledComponent } from "nativewind";
 
 const Container = ({ children }) => {
   return (
-    <StyledComponent component={View} className="p-4 flex-1">
-      {children}
+    <StyledComponent component={ScrollView} className="p-4 flex-1">
+      <View className="pb-8">{children}</View>
     </StyledComponent>
   );
 };
--- client/screens/RoutinesScreen.js
+++ client/screens/RoutinesScreen.js
@@ -1,14 +1,55 @@
-import { Button, Text } from "react-native";
+import { Text, View } from "react-native";
 import Container from "../components/Container";
+import Button from "../components/Button";
+import RoutineList from "../components/RoutineCards/RoutineList";
+
+// Ersätt denna lista med data från API
+const routines = [
+  {
+    id: 1,
+    title: "test1",
+    description: "la alallal",
+    time: "9.00",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    title: "test2",
+    description: "la alallal",
+    time: "9.00",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    title: "test2",
+    time: "9.00",
+    isCompleted: true,
+  },
+];
 
 const RoutinesScreen = ({ navigation }) => {
   return (
     <Container>
-      <Text>Rutiner</Text>
-      <Button
-        title="Redigera rutiner"
-        onPress={() => navigation.navigate("EditRoutines")}
-      />
+      <View className="flex flex-column space-y-10">
+        <Button
+          title="Redigera rutiner (ska flyttas)"
+          onPress={() => navigation.navigate("EditRoutines")}
+        />
+
+        <Text className="text-xl font-bold color-primary100 mb-4">
+          Att göra idag
+        </Text>
+        <RoutineList
+          routines={routines.filter((routine) => !routine.isCompleted)}
+        />
+
+        <Text className="text-xl font-bold color-primary100 mb-4">
+          Utfört idag
+        </Text>
+        <RoutineList
+          routines={routines.filter((routine) => routine.isCompleted)}
+        />
+      </View>
     </Container>
   );
 };
