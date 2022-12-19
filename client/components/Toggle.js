8e6a8837a5dcabcdfcf72e1a840930858d6fc017
ViktoriaNordkvist <61415545+ViktoriaNordkvist@users.noreply.github.com>
2022-12-19 14:17:39 +0100

    Delete Toggle.js

--- client/components/Toggle.js
+++
@@ -1,8 +0,0 @@
-import { Switch } from "react-native";
-import { StyledComponent } from "nativewind";
-
-const Toggle = () => {
-  return <StyledComponent component={Switch} />;
-};
-
-export default Toggle;
