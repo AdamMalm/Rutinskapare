0882f76311c8aba073bf22c4c452c40a8e267bc1
id19vnt <viktorianordkvist@gmail.com>
2022-12-19 14:13:00 +0100

    Add notifications

---
+++ client/components/Toggle.js
@@ -0,0 +1,8 @@
+import { Switch } from "react-native";
+import { StyledComponent } from "nativewind";
+
+const Toggle = () => {
+  return <StyledComponent component={Switch} />;
+};
+
+export default Toggle;
